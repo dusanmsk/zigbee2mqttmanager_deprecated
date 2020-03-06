35974d9c00a67ffd0a804c1425f97a6c10835912
Dusan Zatkovsky <msk.conf@gmail.com>
2020-03-06 17:52:26 +0100

    ...

--- loxone-app/src/main/java/org/msk/loxone/zigbee2mqtt/manager/ui/MainView.java
+++ loxone-app/src/main/java/org/msk/loxone/zigbee2mqtt/manager/ui/MainView.java
@@ -1,10 +1,7 @@
 package org.msk.loxone.zigbee2mqtt.manager.ui;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import javax.annotation.PostConstruct;
 
 @Route
-@SpringComponent
 @RequiredArgsConstructor
 @UIScope
 @Slf4j
